ecc67026aa1575392885e3508271c0d83bc47396
Examnes <examnes@gmail.com>
2022-01-05 20:09:17 +0300

    deleted!

--- assembler/include/rules_provider.h
+++ assembler/include/rules_provider.h
@@ -2,11 +2,16 @@
 #define RULES_PROVIDER_HH
 
 #include "lexer.h"
-
-std::map<state_type,rule> get_rules()
+class rules_provider
 {
-    std::map<state_type,rule> m;
-    m.insert({1,})
-}
+public:
+    static std::map<state_type,rule> get_rules()
+    {
+        std::map<state_type,rule> m;
+        //m.insert({1,[]});
+    }
+};
+
+
 
 #endif // RULES_PROVIDER_HH
